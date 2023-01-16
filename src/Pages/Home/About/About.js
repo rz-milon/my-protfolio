a1f737cf0ef51b4564f03cc7cd99480c47ca3cb3
rzmilon <rzmilon@gmail.com>
2023-01-16 20:52:32 +0600

    modify content

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { FaDownload } from 'react-icons/fa';
+import profileImg from '../../../Assets/profile-image/profile-img.png'
 
 
 const About = () => {
     return (
-        <div className=" min-h-screen text-white">
-            <div className=" hero-content flex flex-col lg:flex-row-reverse mx-48">
-                <img src='' alt='' className="max-w-sm rounded-lg shadow-lg bg-white h-80 opacity-80 hover:opacity-100 transition delay-300" />
+        <div className="hero min-h-screen text-white">
+            <div className=" hero-content flex flex-col lg:flex-row-reverse sm:32 md:36 lg:mx-48">
+                <img src={profileImg} alt='' className="max-w-sm rounded-lg shadow-lg bg-white h-80 opacity-80 hover:opacity-100 transition delay-300" />
                 <div className='min-w-lg'>
-                    <h1 className="text-4xl tracking-tight font-light mt-10 mb-14">About Me</h1>
+                    <h1 className="text-4xl tracking-tight font-light  lg:text-left text-center">About Me</h1>
 
-                    <p className="text-xl tracking-wide leading-7 font-thin"><article>I'm a react front end web developer from Dhaka, Bangladesh.</article>
+                    <p className="text-xl text-left tracking-wide leading-7 font-thin mt-4 lg:mt-12"><article>I'm a react front end web developer from Dhaka, Bangladesh.</article>
                         <article>
                             In previous years I devoted my time to learning Sociology and I have completed my graduation.</article> <article>Always hungry to keep learning.
                                 I like dabbling in various parts of front-end development and like to learn about new technologies.</article>
